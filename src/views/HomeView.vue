05d403aa7fae4d2a2da5c484ec2d9968bc6cd707
xtance <ni77ty@gmail.com>
2023-12-17 12:51:50 +0300

    подробнее об опыте

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -45,46 +45,52 @@
 	</div>
 
 	<div class="bg-gradient-to-b from-white/5 rounded-t-md mx-2 lg:mx-0">
-		<Heading class="p-4 border-b border-white/5">Опыт разработки проектов</Heading>
+		<div class="p-4 border-b border-white/5">
+			<Heading class="pb-2">Опыт разработки проектов</Heading>
+			<RouterLink to="/portfolio" class="flex items-center gap-2 animate-pulse">
+				<i class="mi">arrow_forward_ios</i>
+				<div>Подробнее в портфолио</div>
+			</RouterLink>
+		</div>
 
 		<TextExperience>
 			<template v-slot:year>2022-2023</template>
-			<template v-slot:info>Разработка веб-панели</template>
+			<template v-slot:info>Разрабатывал панель управления серверами для администраторов и игроков на SvelteKit.</template>
 		</TextExperience>
 
 		<TextExperience>
 			<template v-slot:year>2022</template>
-			<template v-slot:info>Участие в WorldSkills VI</template>
+			<template v-slot:info>Участвовал в региональном этапе WorldSkills в компетенции "Веб-технологии" (2 место).</template>
 		</TextExperience>
 
 		<TextExperience>
 			<template v-slot:year>2022</template>
-			<template v-slot:info>Вёрстка макета планшета</template>
+			<template v-slot:info>Верстал интерфейс по макету Figma для немецкого проекта Cloud RP на Vue + Tailwind.</template>
 		</TextExperience>
 
 		<TextExperience>
 			<template v-slot:year>2021-2022</template>
-			<template v-slot:info>Разработка сайта мебельного салона</template>
+			<template v-slot:info>Работал над сайтом мебельного салона в Санкт-Петербурге на Vue 3, с адаптивной CSS-вёрсткой.</template>
 		</TextExperience>
 
 		<TextExperience>
 			<template v-slot:year>2021</template>
-			<template v-slot:info>Практика в ГУП ИАЦ</template>
+			<template v-slot:info>Проходил практику в Санкт-Петербургском информационно-аналитическом центре (СПб ИАЦ), работал с базами данных.</template>
 		</TextExperience>
 
 		<TextExperience>
 			<template v-slot:year>2020</template>
-			<template v-slot:info>Сервер alt:V</template>
+			<template v-slot:info>Разрабатывал интерфейс для сервера alt:V с помощью CEF, на фреймворке Svelte 3. <br>Построено взаимодействие с серверной частью на событиях, реализован механизм RPC.</template>
 		</TextExperience>
 
 		<TextExperience>
 			<template v-slot:year>2018-2022</template>
-			<template v-slot:info>Проекты в колледже</template>
+			<template v-slot:info>Проходил обучение в Политехническом Колледже Городского Хозяйства (СПб ГБПОУ «ПКГХ»). <br>В основном изучал веб-технологии, но был практический опыт с C# и C++.</template>
 		</TextExperience>
 
 		<TextExperience>
 			<template v-slot:year>2017</template>
-			<template v-slot:info>Сервер в CS</template>
+			<template v-slot:info>Начал знакомство с программированием с плагинов на SourcePawn, скриптов на PHP. <br>Изучал SQL-запросы. Занимался в основном бэкендом, в дальнейшем решил перейти на фронтэнд и веб-дизайн</template>
 		</TextExperience>
 		
 	</div>
