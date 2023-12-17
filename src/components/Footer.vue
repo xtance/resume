32528e6642028105e3c37826941121e2045553fd
xtance <ni77ty@gmail.com>
2023-12-17 14:04:07 +0300

    footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,14 @@
 <template>
-	<footer class="my-16 flex justify-center gap-2">
-		<div class="bold opacity-80">xtance</div>
-		<div>•</div>
-		<div class="light">2001</div>
+	<footer class="my-16">
+		<div class="flex justify-center gap-2">
+			<div class="bold opacity-80">xtance</div>
+			<div>•</div>
+			<div class="light">2001</div>
+		</div>
+		<div class="opacity-70 text-center">
+			Исходный код портфолио <a class="underline underline-offset-4 decoration-dotted" href="https://github.com/xtance/resume">также доступен на GitHub</a> 
+		</div>
+		
 	</footer>
 </template>
 
