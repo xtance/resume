a456341a01f0f716577d3881c50a5c59edf34ba5
xtance <ni77ty@gmail.com>
2023-12-17 13:55:37 +0300

    иконка времени

--- src/components/text/TextExperience.vue
+++ src/components/text/TextExperience.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="p-4 border-b border-white/5">
-		<div class="opacity-80 bold">
+		<div class="opacity-80 bold flex gap-2 items-center pb-2">
+			<i class="mi text-xl">today</i>
 			<slot name="year"/>
 		</div>
 		<div>
